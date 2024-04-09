cf10fb7d2dc37e3470ff18060d273026b2fa234e
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-04-09 00:28:16 -0300

    feat: search bar view toggling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,22 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 
 interface MenuOption {
 	child: ReactElement;
 	loginRequired: boolean;
 }
 
-let isLogged: boolean = true;
-
 const menuOptions: MenuOption[] = [
 	{ child: <li>Home</li>, loginRequired: false, },
-	{ child: <li>Categorias</li>, loginRequired: false, },
+	{
+		child: <li>Categorias
+			<span>{'>'}</span>
+			<ul>
+				<li><a href="">Maquiagem</a></li>
+				<li><a href="">Cosméticos</a></li>
+				<li><a href="">Perfumes</a></li>
+			</ul>
+		</li>, loginRequired: false,
+	},
 	{ child: <li>Sessão VIP</li>, loginRequired: false, },
 	{ child: <li>Ofertas</li>, loginRequired: false, },
 	{ child: <li>Marcas</li>, loginRequired: false, },
@@ -18,45 +25,71 @@ const menuOptions: MenuOption[] = [
 	{ child: <li>Sair</li>, loginRequired: true, },
 ]
 
+
 export function Navbar() {
+	let [isLogged, setLogin] = useState(false);
+	let [showSearchBar, setSearchBarView] = useState(true);
+	
+	function toggleSearchBarView() {
+		setSearchBarView(!showSearchBar);
+	}
 
 	return (
 		<>
-			<header className="bg-white border-b-2">
+			<header className="bg-slate-50 border-b-2">
 				<nav>
-					{/* Menu button */}
-					<div id="menu-toggler">
-						<input type="checkbox" id="toggle" />
-						<label htmlFor="toggle">
-							<i className="fa-solid fa-bars fa-lg"></i>
-						</label>
-					</div>
+					{/* navbar top */}
+					<div>
 
-					{/* Logo */}
-					<a href="">Makeshop</a>
-
-
-					<div id="menu">
-						<div className="bg-black">
-							<h2>Bem-vindo</h2>
-							<p>Faça login ou cadastre-se para aproveitar as nossas ofertas </p>
-
-							<div>
-								<a href="">Fazer Login</a>
-								<a href="">Cadastre-se</a>
-							</div>
+						{/* Menu button */}
+						<div id="menu-toggler">
+							<input type="checkbox" id="toggle" />
+							<label htmlFor="toggle">
+								<i className="fa-solid fa-bars fa-lg"></i>
+							</label>
 						</div>
 
-						<ul>
-							{menuOptions.map((menuOption: MenuOption) => {
-								if (menuOption.loginRequired && !isLogged) {
-									return <></>;
-								}
-								return menuOption.child;
-							})}
-						</ul>
+						{/* Logo */}
+						<a href="">Makeshop</a>
+
+						{/* Menu */}
+						<div id="menu">
+							<div className="bg-black">
+								<h2>Bem-vindo</h2>
+								<p>Faça login ou cadastre-se para aproveitar as nossas ofertas </p>
+
+								<div>
+									<a href="">Fazer Login</a>
+									<a href="">Cadastre-se</a>
+								</div>
+							</div>
+
+							{/* Menu options*/}
+							<ul>
+								{menuOptions.map((menuOption: MenuOption) => {
+									if (menuOption.loginRequired && !isLogged) {
+										return <></>;
+									}
+									return menuOption.child;
+								})}
+							</ul>
+						</div>
+
+
+						<div>
+							<span onClick={toggleSearchBarView}>lupa</span>
+							<div></div>
+							<span>kart</span>
+						</div>
 					</div>
 
+					{/* navbar bottom */}
+					{showSearchBar ? <form id="search-form" className="">
+						<input type="search" id="input-search" placeholder="Pesquise produtos e marcas" />
+						<label htmlFor="input-search">
+							<span>Lupa</span>
+						</label>
+					</form> : <></>}
 				</nav>
 			</header>
 		</>
