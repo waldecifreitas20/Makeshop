1daf56dcee1bf19d464dadb6107bed8c55e70b94
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-04-17 15:41:35 -0300

    feat: slider next v.05

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { EventHandler } from "react";
 
 interface CarouselProps {
     items: Array<any>;
@@ -7,30 +7,44 @@ interface CarouselProps {
 
 export class Carousel extends React.Component {
     private key: string;
-    private viewportWidth: number;
+    private viewWidth: number;
+    private index: number;
+    private next;
 
     public props: CarouselProps;
 
+
     public constructor(props: CarouselProps) {
         super(props);
-        this.key = Math.random().toString();
+        this.key = (Math.random()).toString();
         this.props = props;
-        this.viewportWidth = -1;
+        this.index = 0;
+        this.viewWidth = -1;
+
+
+        this.next = () => {
+            this.index++;
+
+            const slider = this.getSlider();
+            slider.style.left = `-${this.viewWidth * this.index}px`;
+
+            console.log(slider.style.left);
+
+        }
     }
 
     public render() {
-
         window.addEventListener("resize", () => {
             this.updateCarouselItemsWidth();
         });
 
         return <>
 
-            <div className="carousel flex justify-between px-5">
+            <div id={`carousel-${this.key}`} className="flex justify-between px-5">
                 <button className="" onClick={this.prev}>Voltar</button>
 
-                <div id={`carousel-view-${this.key}`} className="relative flex overflow-hidden w-[95%]">
-                    <ul id={`slider-${this.key}`} className="slider transition-all duration-700 flex">
+                <div id={`carousel-view-${this.key}`} className="relative flex overflow-hidden w-[90%]">
+                    <ul id={`slider-${this.key}`} className="slider relative transition-all duration-700 flex">
                         {this.props.items.map((item, i) => {
                             return <>
                                 <li className={`carousel-item-${this.key}`}>{item}</li>
@@ -47,30 +61,32 @@ export class Carousel extends React.Component {
 
     public componentDidMount() {
         this.updateCarouselItemsWidth();
-    }
-    private updateCarouselItemsWidth() {
-        const carouselViewPortWidth = document.getElementById(`carousel-view-${this.key}`)?.offsetWidth ?? -1;
-        this.setItemsWidth(carouselViewPortWidth);
+
+        const slider = this.getSlider();
+
+        slider.style.left = '0px';
     }
 
-    private setItemsWidth(width: number) {
+    private updateCarouselItemsWidth() {
+        const carouselViewPortWidth = document
+            .getElementById(`carousel-view-${this.key}`)?.offsetWidth ?? -1;
+
         const items = document
             .getElementsByClassName(`carousel-item-${this.key}`) as HTMLCollectionOf<HTMLElement>;
 
-        console.log(width);
-
+        this.viewWidth = carouselViewPortWidth;
         for (const item of items) {
-            item.style.width = `${width}px`;
+            item.style.width = `${carouselViewPortWidth}px`;
         }
-    }
-
-    private next() {
-
     }
 
     private prev() {
 
     }
 
+
+    private getSlider(): HTMLElement {
+        return document.getElementById(`slider-${this.key}`) as HTMLElement;
+    }
 
 }
