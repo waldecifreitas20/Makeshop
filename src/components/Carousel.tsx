5ad8f5fbe7402a5340d5ac43b0edcfadfd138a6b
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-04-17 15:01:44 -0300

    feat: auto-resize carousel items width

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,47 +7,68 @@ interface CarouselProps {
 
 export class Carousel extends React.Component {
     private key: string;
+    private viewportWidth: number;
 
-    public props : CarouselProps;
+    public props: CarouselProps;
 
-    constructor(props: CarouselProps) {
+    public constructor(props: CarouselProps) {
         super(props);
         this.key = Math.random().toString();
         this.props = props;
+        this.viewportWidth = -1;
     }
 
-    render() {
+    public render() {
+
+        window.addEventListener("resize", () => {
+            this.updateCarouselItemsWidth();
+        });
+
         return <>
 
-              <div className="carousel flex justify-between px-5">
+            <div className="carousel flex justify-between px-5">
                 <button className="" onClick={this.prev}>Voltar</button>
 
-                <div id={`carousel-view-${this.key}`} className="relative flex overflow-hidden w-[300px]">
+                <div id={`carousel-view-${this.key}`} className="relative flex overflow-hidden w-[95%]">
                     <ul id={`slider-${this.key}`} className="slider transition-all duration-700 flex">
-                       {this.props.items.map((item, i) => {
-                        return <>
-                            <li className="w-[300px]">{item}</li>
-                        </>
-                       })}
+                        {this.props.items.map((item, i) => {
+                            return <>
+                                <li className={`carousel-item-${this.key}`}>{item}</li>
+                            </>
+                        })}
                     </ul>
                 </div>
 
                 <button onClick={this.next}>Proxima</button>
-            </div> 
+            </div>
         </>;
 
     }
 
-    componentDidMount() {
-        const carouselView = document.getElementById(`carousel-view-${this.key}`);
-        console.log(carouselView?.offsetWidth);
+    public componentDidMount() {
+        this.updateCarouselItemsWidth();
+    }
+    private updateCarouselItemsWidth() {
+        const carouselViewPortWidth = document.getElementById(`carousel-view-${this.key}`)?.offsetWidth ?? -1;
+        this.setItemsWidth(carouselViewPortWidth);
     }
 
-    next() {
+    private setItemsWidth(width: number) {
+        const items = document
+            .getElementsByClassName(`carousel-item-${this.key}`) as HTMLCollectionOf<HTMLElement>;
+
+        console.log(width);
+
+        for (const item of items) {
+            item.style.width = `${width}px`;
+        }
+    }
+
+    private next() {
 
     }
 
-    prev() {
+    private prev() {
 
     }
 
