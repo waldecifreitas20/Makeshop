e878459750b9a17c4376480ef35a589b594ba6d0
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-04-18 12:26:21 -0300

    feat: carousel floating buttons

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,10 +6,20 @@ interface CarouselProps {
 }
 
 
+const FLOAT_BUTTON_STYLE = `
+    absolute
+    z-index
+    h-full
+    block
+    border border-blue-500
+
+`;
+
 export class Carousel extends React.Component {
     private readonly key: string;
     private readonly nextItem: VoidFunction;
     private readonly previousItem: VoidFunction;
+    private readonly enableFloatingButtons: boolean;
     private viewWidth: number;
     private index: number;
     private hasEventTriggered: boolean;
@@ -25,6 +35,7 @@ export class Carousel extends React.Component {
         this.index = 0;
         this.viewWidth = -1;
         this.hasEventTriggered = false;
+        this.enableFloatingButtons = true;
 
         this.nextItem = (): void => {
             const ITEMS_QTD = this.props.items.length;
@@ -58,37 +69,44 @@ export class Carousel extends React.Component {
 
         return <>
 
-            <div id={`carousel-${this.key}`} className="flex justify-between px-5">
-                <button className="" onClick={() => {
+            <div id={`carousel-${this.key}`} className="flex justify-center relative">
+
+                <button className={`absolute left-0 h-full ms-2 z-20`} onClick={() => {
                     this.hasEventTriggered = true;
                     this.previousItem();
 
                     this.hasEventTriggered = false;
-                    
                 }
-                }>Voltar</button>
+                }>
+                    <span className="block  bg-black  transition-all duration-300 opacity-20 hover:opacity-60 text-white rounded-full p-4">
+                        <i className="fa-solid fa-chevron-left fa-2xl"></i>
+                    </span>
+                </button>
 
-                <div id={`carousel-view-${this.key}`} className="relative flex overflow-hidden w-[90%]">
+                <div id={`carousel-view-${this.key}`} className="flex overflow-hidden w-[500px] border border-green-500">
                     <ul id={`slider-${this.key}`} className="slider relative transition-all duration-1000 flex h-full">
                         {this.props.items.map((item, i) => {
                             return <>
-                                <li className={`carousel-item-${this.key} text-center h-[200px] flex justify-center`}>{item}</li>
+                                <li className={`carousel-item-${this.key} text-center flex justify-center w-full`}>{item}</li>
                             </>
                         })}
                     </ul>
                 </div>
 
-                <button onClick={() => {
+                <button className={`absolute right-0 h-full ms-2 z-20`} onClick={() => {
                     this.hasEventTriggered = true;
                     this.nextItem();
                     this.hasEventTriggered = false;
                 }
-                }>Proxima</button>
+                }>
+                    <span className="block  bg-black  transition-all duration-300 opacity-20 hover:opacity-60 text-white rounded-full p-4">
+                        <i className="fa-solid fa-chevron-right fa-2xl"></i>
+                    </span>
+                </button>
             </div>
         </>;
 
     }
-
 
     public componentDidMount(): void {
         this.getSlider().style.left = '0px';
@@ -111,8 +129,7 @@ export class Carousel extends React.Component {
 
 
     private runAutoPlay() {
-        const autoPlayId = setInterval(() => {
-
+        setInterval(() => {
             if (!this.hasEventTriggered) {
                 this.nextItem();
             }
