9c8feff17353a46da18363fb175e67e840055510
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-04-21 14:35:06 -0300

    fix: carousel button allowing  click outside

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -9,8 +9,10 @@ interface FloatingButtonProps {
 
 export function IconButton(props: FloatingButtonProps) {
     return <>
-        <button className={`absolute ${props.positionClass} h-full ms-2 z-20`} onClick={() => props.onClick()}>
-            <span className="bg-black  transition-all duration-300 h-12 w-12 flex items-center justify-center bg-opacity-20 hover:bg-opacity-60 text-white rounded-full p-4">
+        <button className={`absolute ${props.positionClass} h-full ms-2 z-20 cursor-default`} >
+            <span className="bg-black cursor-pointer transition-all duration-300 h-12 w-12 flex items-center justify-center bg-opacity-20 hover:bg-opacity-60 text-white rounded-full p-4"
+                onClick={() => props.onClick()}
+            >
                 {props.child}
             </span>
         </button>
