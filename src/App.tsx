07ba1ededed0c0a86dfc4036244e17c741922cd3
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-04-24 16:52:43 -0300

    feat: add payment methods images to footer

--- src/App.tsx
+++ src/App.tsx
@@ -254,25 +254,25 @@ export function App() {
                 </div>
                 <div className="my-7">
                     <h4 className="text-xl mb-3 font-medium">Redes Sociais</h4>
-                    <ul className="flex justify-around">
-                        <li><a className="capitalize" href=""><img src="./assets/images/logo-fb.png" alt="" /></a></li>
-                        <li><a className="capitalize" href=""><img src="./assets/images/logo-fb.png" alt="" /></a></li>
-                        <li><a className="capitalize" href=""><img src="./assets/images/logo-fb.png" alt="" /></a></li>
-                        <li><a className="capitalize" href=""><img src="./assets/images/logo-fb.png" alt="" /></a></li>
-                        <li><a className="capitalize" href=""><img src="./assets/images/logo-fb.png" alt="" /></a></li>
+                    <ul className="flex justify-between max-w-80">
+                        <li><a className="h-8 block" href=""><img className="block h-full" src="assets/images/logo-fb.png" alt="" /></a></li>
+                        <li><a className="h-8 block" href=""><img className="block h-full" src="assets/images/logo-insta.png" alt="" /></a></li>
+                        <li><a className="h-8 block" href=""><img className="block h-full" src="./assets/images/logo-ttk.png" alt="" /></a></li>
+                        <li><a className="h-8 block" href=""><img className="block h-full" src="./assets/images/logo-x.png" alt="" /></a></li>
+                        <li><a className="h-8 block" href=""><img className="block h-full" src="./assets/images/logo-yt.png" alt="" /></a></li>
                     </ul>
                 </div>
                 
                 <div className="my-7">
                     <h4 className="text-xl mb-3 font-medium">Formas de Pagamento</h4>
-                    <ul className="flex flex-wrap justify-left">
-                        <li className="block bg-blue-500"><a className="border-2 border-sky-600 blockcapitalize" href="">mercado pago</a></li>
-                        <li className="block bg-blue-500"><a className="border-2 border-sky-600 blockcapitalize" href="">mercado pago</a></li>
-                        <li className="block bg-blue-500"><a className="border-2 border-sky-600 blockcapitalize" href="">mercado pago</a></li>
-                        <li className="block bg-blue-500"><a className="border-2 border-sky-600 blockcapitalize" href="">mercado pago</a></li> 
-                        <li className="block bg-blue-500"><a className="border-2 border-sky-600 blockcapitalize" href="">mercado pago</a></li>
-                        <li className="block bg-blue-500"><a className="border-2 border-sky-600 blockcapitalize" href="">mercado pago</a></li>
-                        <li className="block bg-blue-500"><a className="border-2 border-sky-600 blockcapitalize" href="">mercado pago</a></li>
+                    <ul className="flex flex-wrap justify-left gap-4">
+                        <li className=""><a className="h-8 block" href=""><img className="block h-full" src="../public/assets/images/pg-mastercard.png" alt="" /></a></li>
+                        <li className=""><a className="h-8 block" href=""><img className="block h-full" src="../public/assets/images/pg-visa.png" alt="" /></a></li>
+                        <li className=""><a className="h-8 block" href=""><img className="block h-full" src="../public/assets/images/pg-amex.png" alt="" /></a></li>
+                        <li className=""><a className="h-8 block" href=""><img className="block h-full" src="../public/assets/images/pg-elo.png" alt="" /></a></li> 
+                        <li className=""><a className="h-8 block" href=""><img className="block h-full" src="../public/assets/images/pg-pix.png" alt="" /></a></li>
+                        <li className=""><a className="h-8 block" href=""><img className="block h-full" src="../public/assets/images/pg-paypal.png" alt="" /></a></li>
+                        <li className=""><a className="h-8 block" href=""><img className="block h-full" src="../public/assets/images/pg-mpago.png" alt="" /></a></li>
                         
                     </ul>
                 </div>
