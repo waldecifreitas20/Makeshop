2a885a236e674ed2fcbd3d71a59ea57297c77bcf
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-01 17:04:10 -0300

    refactor: menu options and searchbar for large devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -82,15 +82,12 @@ export function Navbar() {
 		setMenuClass('close-menu');
 	}
 
-	
-
-
 	return (
 		<>
 			<header className="fixed top-0 z-50 w-full bg-zinc-50">
-				<nav className="border-b-2 py-4 px-5 z-50 ">
+				<nav className="border-b-2 pt-4 pb-6 px-5 z-50">
 					{/* navbar top */}
-					<div className="flex items-center">
+					<div className="flex items-center lg:h-4 lg:inline">
 
 						{/* Menu button */}
 						<button onClick={openMenu} className={`
@@ -103,99 +100,11 @@ export function Navbar() {
 
 
 						{/* Logo */}
-						<a href="" className="text-3xl font-title tracking-tightest text-gray-650">Makeshop</a>
+						<a href="" className="
+						text-3xl font-title tracking-tightest text-gray-650 
+						float-left lg:text-4xl">Makeshop</a>
 
-						{/* Menu */}
-						<div id="menu" className={`
-						shadow-2xl 
-						z-40 
-						block 
-						absolute 
-						w-full 
-						transition-all duration-500 
-						${menuClass}
-						
-						lg:relative
-						lg:h-6
-						lg:open-menu
-					
-						`}>
-
-							{/* close menu when user clicks outside menu area */}
-							<div onClick={closeMenu} className="lg:hidden menu-disposer block relative bg-black bg-opacity-25 h-screen w-full"></div>
-
-							<div className="
-							absolute 
-							z-50
-							hide-scrollbar 
-							top-0 left-0 
-							bg-white 
-							overflow-y-auto 
-							h-screen w-3/4 
-							block 
-							
-							lg:block
-							lg:w-full
-							lg:h-[fit-content]
-							lg:bg-transparent
-							">
-								{/* menu header */}
-								<div className="
-								bg-zinc-950 
-								px-5 pt-16 pb-6 
-								text-center 
-								relative z-10
-
-								lg:hidden
-								">
-									<h2 className="text-white text-xl">Bem-vindo!</h2>
-									<p className="text-white text-sm mt-2">Faça login ou cadastre-se para aproveitar as nossas ofertas </p>
-
-									<div className="mt-4">
-										<a className="
-									block py-2 mb-2 
-									rounded-full
-									border 
-									border-pink-400  
-									hover:bg-pink-400 
-									text-white 
-									
-									transition-all duration-300" href="">Fazer Login</a>
-										<a className="
-									block py-2 
-									text-white  
-									rounded-full 
-									border 
-									border-pink-500 
-									hover:border-pink-500 
-									bg-pink-500 
-									hover:bg-pink-500 
-
-									transition-all duration-300 " href="">Cadastre-se</a>
-									</div>
-								</div>
-
-								{/* Menu options*/}
-								<ul className="
-								-z-0 relative
-
-								lg:flex
-								lg:justify-center
-								">
-									{MENU_OPTIONS.map((menuOption: MenuOption) => {
-										if (menuOption.loginRequired && !isLogged) {
-											return <></>;
-										}
-										return menuOption.child;
-									})}
-								</ul>
-							</div>
-
-						</div>
-
-
-
-						<div className="ml-auto flex">
+						<div className="ml-auto flex lg:hidden">
 							<button onClick={toggleSearchBarView}>
 								<i className="fa-solid fa-magnifying-glass fa-xl"></i>
 							</button>
@@ -206,17 +115,143 @@ export function Navbar() {
 						</div>
 					</div>
 
-					{/* navbar bottom */}
-					{showSearchBar ? <form id="search-form" className="
-					bg-white mt-5 border-2 h-12 flex items-center justify-between px-5 rounded-full focus-within:border-fuchsia-300 transition-all duration-300
+					<div className="
 
-					lg:block
+					flex items-center justify-between 
+					
+					lg:relative 
+					lg:w-[73%] 
+					lg:ml-auto 
+				
 					">
-						<input className="h-full w-full outline-none text-lg mr-4" type="search" id="input-search" placeholder="Pesquise produtos e marcas" />
-						<button>
-							<i className="fa-solid fa-magnifying-glass fa-lg text-gray-500"></i>
-						</button>
-					</form> : <></>}
+						
+						{/*search bar */}
+						{true ? <form id="search-form" className="
+						bg-white 
+						border-2 
+						mt-5 h-12 px-5 
+						w-full
+						flex items-center justify-between 
+						rounded-full 
+						focus-within:border-fuchsia-300 
+						transition-all duration-300
+
+						lg:w-[63%]
+						lg:h-10	
+						lg:m-0
+					">
+							<input className="
+							h-full w-full outline-none 
+							text-lg
+							mr-4
+
+							lg:text-sm
+							" type="search" id="input-search" placeholder="Pesquise produtos e marcas" />
+							<button>
+								<i className="fa-solid fa-magnifying-glass fa-lg text-gray-500"></i>
+							</button>
+						</form> : <></>}
+
+						<div className="hidden lg:flex w-[30%] bg-blue-500 justify-between">
+							<div>dasjkdlaskd-654654</div>
+							<div>dasjkdlaskd-654654</div>
+							<div>dasjkdlaskd-654654</div>
+
+						</div>
+					</div>
+
+					{/* Menu panel*/}
+					<div id="menu" className={`
+						shadow-2xl 
+						z-40 
+						block 
+						absolute 
+						top-0
+						w-full 
+						transition-all duration-500 
+						${menuClass}
+						
+						lg:relative
+						lg:h-6
+						lg:open-menu
+					
+					
+						`}>
+
+						{/* close menu when user clicks outside menu area */}
+						<div onClick={closeMenu} className="lg:hidden menu-disposer block relative bg-black bg-opacity-25 h-screen w-full"></div>
+
+						{/* menu */}
+						<section className="
+							absolute 
+							z-50
+							hide-scrollbar 
+							top-0 left-0 
+							bg-white 
+							overflow-y-auto 
+							h-screen w-3/4 
+							block 
+							
+							lg:block
+							lg:py-4
+							lg:bg-transparent
+							lg:w-full
+							lg:h-[fit-content]
+							">
+
+							{/* menu header */}
+							<div className="
+								bg-zinc-950 
+								px-5 pt-16 pb-6 
+								text-center 
+								relative z-10
+
+								lg:hidden
+								">
+								<h2 className="text-white text-xl">Bem-vindo!</h2>
+								<p className="text-white text-sm mt-2">Faça login ou cadastre-se para aproveitar as nossas ofertas </p>
+
+								<div className="mt-4">
+									<a className="
+									block py-2 mb-2 
+									rounded-full
+									border 
+									border-pink-400  
+									hover:bg-pink-400 
+									text-white 
+									
+									transition-all duration-300" href="">Fazer Login</a>
+									<a className="
+									block py-2 
+									text-white  
+									rounded-full 
+									border 
+									border-pink-500 
+									hover:border-pink-500 
+									bg-pink-500 
+									hover:bg-pink-500 
+
+									transition-all duration-300 " href="">Cadastre-se</a>
+								</div>
+							</div>
+
+							{/* Menu options*/}
+							<ul className="
+								-z-0 relative
+
+								lg:flex
+								lg:justify-center
+								">
+								{MENU_OPTIONS.map((menuOption: MenuOption) => {
+									if (menuOption.loginRequired && !isLogged) {
+										return <></>;
+									}
+									return menuOption.child;
+								})}
+							</ul>
+						</section>
+
+					</div>
 				</nav>
 			</header>
 		</>
