53677ec63da3b91c263630a2699b98138bd60618
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-01 10:25:04 -0300

    fix: responsible banner cuppom

--- src/App.tsx
+++ src/App.tsx
@@ -30,11 +30,11 @@ export function App() {
     }
 
     const BANNERS = [
-        <span className="relative bg-green-500 block mx-auto h-full w-full">
+        <span className="relative block mx-auto h-full w-full">
             <img className="block w-full h-full mx-auto" src={`./assets/images/banner1${isBigBanner ? "-big" : ""}.png`} alt="" />
 
-            <div className="w-full flex justify-center">
-                <div className="absolute bottom-2 w-2/4 flex self-center justify-center">
+            <div className="w-full absolute bottom-2 lg:bottom-6 flex z-50 justify-center md:justify-end">
+                <div className="w-2/4 sm:w-[40%] lg:w-[30%] flex justify-center md:mr-[15%] lg:mr-[20%]">''
                     <input type="text"
                         value="MAKE15OFF"
                         readOnly disabled
@@ -47,6 +47,10 @@ export function App() {
                             border-y-2 border-l-2 border-dashed border-gray-100
                             text-white text-center
                             bg-white bg-opacity-20 
+                            
+                            md:text-xl
+                            lg:text-2xl
+                         
                     "/>
                     <button className="bg-white hover:bg-gray-200 text-pink-600 border rounded-r-md p-2 text-sm w-1/4" onClick={() => copyToClipboard("MAKE15OFF")}>
                         {buttonCopyIcon}
