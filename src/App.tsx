58481fbb0ca13c3268f3e3a6746a9fd1451cb5e2
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-07 12:09:54 -0300

    refactor: newsletter section for large devices

--- src/App.tsx
+++ src/App.tsx
@@ -294,26 +294,43 @@ export function App() {
                 text-zinc-200 text-opacity-95 text-sm
                 py-6 px-5
                 mt-8
+
+                lg:p-10
+                lg:px-[100px]
+                lg:text-lg
                 "
-                child={<>
+                child={
+                <div className="flex items-center h-12 mt-10 gap-5">
                     <input
                         type="email"
                         placeholder="Digite seu email"
-                        className="w-full rounded-full px-5 py-2 mt-4 mb-3 text-black text-lg"
+                        className="
+                        w-full border-0 rounded-full px-5 py-2 mt-4 mb-3 text-black text-lg
+                        lg:h-full
+                        lg:py-0
+                        "
                     />
 
-                    <PillButton
+                    <Pill
                         text="Cadastrar"
                         style="
+                        border-0
                         bg-zinc-600
                         text-lg
                         text-center 
                         w-48 
                         ml-auto
                         hover:bg-zinc-800
+                        transition-all
+                        duration-300
+
+                        lg:h-full
+                        lg:w-72
+                        lg:my-0
+                        lg:text-lg
                         "
                     />
-                </>}
+                </div>}
             />
 
             <Footer />
