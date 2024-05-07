b4983a04d17c844cb7ee47f9a01aa7e3269e8a03
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-07 10:44:39 -0300

    feat: react children function tool

---
+++ src/utils/react.tsx
@@ -0,0 +1,11 @@
+import { Children, ReactElement } from "react";
+
+function renderElementChildren(children: any): ReactElement {
+    return <>
+        {Children.map(children, (child, i) => {
+            return <>{child}</>;
+        })}
+    </>;
+}
+
+export { renderElementChildren }
