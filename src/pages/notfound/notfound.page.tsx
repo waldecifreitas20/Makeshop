f0d82096865c1d60b20cabe68d7dda1b25a44093
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-21 10:44:02 -0300

    feat: create notfound page

---
+++ src/pages/notfound/notfound.page.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "../../components/Navbar";
+
+export function NotFoundPage() {
+  return <>
+
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-center">
+        <img className="mx-auto" src="https://cdn.pixabay.com/photo/2017/02/12/21/29/false-2061132_640.png" width={150} alt="" />
+        <h2 className="text-4xl md:text-6xl font-bold tracking-tightest">Erro 404 - Not Found</h2>
+        <p className="text-xl mb-8">A página que você procura não existe</p>
+        <a className="block max-w-[300px] mx-auto mt-10 text-white bg-pink-500 hover:bg-pink-600 border-black px-3 py-2 rounded-full" href="/">Ir para Home</a>
+      </div>
+    </div>
+  </>
+
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "../pages/home/Home.page";
+import { NotFoundPage } from "../pages/notfound/notfound.page";
 
 export const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
+  { path: '*', element: <NotFoundPage /> },
 ]);
