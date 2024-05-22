67d802e37ed267d9dc7a8c58d09e523f7779b6ee
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-22 11:03:48 -0300

    feat: responsible login page

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -3,15 +3,43 @@ import { routes } from "../../routes/routes";
 
 export function LoginPage() {
   const styles = {
+    loginCard: `
+      md:mt-8
+      md:bg-white
+      md:border
+      md:w-[400px] 
+      md:rounded-lg
+      md:mx-auto 
+      md:py-10 md:px-8
+    `,
     input: `
       block 
       border border-zinc-400 rounded-full 
       w-full 
-      px-5 py-3 
+      px-5 py-2 
       my-2
       text-lg
-    `,
+
+      md:py-2
+      md:text-md
+
+      md:text-sm
+      md:px-2
+      lg:rounded-md
+      `,
     loginButton: `
+      mt-4 mb-2 
+      bg-neutral-800 hover:bg-black 
+      py-2 
+      text-xl text-white 
+      transition-all duration-300  
+      
+      md:text-sm
+      
+      lg:mt-12
+      lg:rounded-md
+      `,
+    externalLoginButton: `
       bg-white
       border
       block
@@ -22,7 +50,7 @@ export function LoginPage() {
       capitalize
       w-[300px]
       hover:bg-zinc-100
-    `,
+      `,
     icon: `
       block
       size-6
@@ -40,16 +68,16 @@ export function LoginPage() {
         </div>
       </header>
 
-      <main className="mt-16">
-        <h1 className="text-center text-3xl mb-10">Faça login e aproveite nossas ofertas</h1>
+      <main className={`mt-16 mx-auto max-w-[400px] ${styles.loginCard}`}>
+        <h1 className="text-center text-3xl mb-10 lg:text-2xl">Faça login e aproveite nossas ofertas</h1>
 
-        <form action="">
+        <form>
           <input className={styles.input} required placeholder="Email" type="email" />
           <input className={styles.input} required placeholder="Senha" type="password" />
-          <a className="block ml-auto w-fit mr-5" href="">Esqueci minha senha</a>
+          <a className="block ml-auto w-fit mr-5 text-sm" href="">Esqueci minha senha</a>
 
-          <Pill style="mt-4 mb-2 bg-neutral-800 hover:bg-black py-3 text-xl text-white transition-all duration-300" text="Entrar"/>
-          <a className="block mx-auto w-fit" href="">Não tenho cadastro</a>
+          <Pill style={styles.loginButton} text="Entrar" />
+          <a className="block mx-auto w-fit text-sm" href="">Não tenho cadastro</a>
 
           {/* Future feature 
           <div className="mt-8 mb-3 relative">
@@ -58,15 +86,15 @@ export function LoginPage() {
           </div>
 
           <div>
-            <button className={styles.loginButton}>
+            <button className={styles.externalLoginButton}>
               <span>Fazer login com google</span>
               <img className={styles.icon} src="./images/icon-google.png" alt="" />
             </button>
-            <button className={styles.loginButton}>
+            <button className={styles.externalLoginButton}>
               <span>Fazer login com facebook</span>
               <img className={styles.icon} src="./images/icon-facebook.svg" alt="" />
             </button>
-            <button className={styles.loginButton}>
+            <button className={styles.externalLoginButton}>
               <span>Fazer login com X</span>
               <img className={styles.icon} src="./images/icon-x.png" alt="" />
             </button>
