c9ab4db090add22982e048b58aac97f77f2a77f1
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-22 12:39:44 -0300

    feat: create singup page

---
+++ src/pages/singup/sing_up.page.tsx
@@ -0,0 +1,10 @@
+import { BackHomeButton } from "../../components/BackHomeButton";
+
+export function SingUpPage() {
+  return <>
+    <div className="px-8 pt-10">
+      <BackHomeButton />
+
+    </div>
+  </>;
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,9 +4,11 @@ import { HomePage } from "../pages/home/Home.page";
 import { NotFoundPage } from "../pages/notfound/notfound.page";
 import { routes } from "./routes";
 import { LoginPage } from "../pages/login/login.page";
+import { SingUpPage } from "../pages/singup/sing_up.page";
 
 export const router = createBrowserRouter([
   { path: routes.home, element: <HomePage /> },
   { path: routes.login, element: <LoginPage /> },
+  { path: routes.singUp, element: <SingUpPage /> },
   { path: routes.notfound, element: <NotFoundPage /> },
 ]);
