04a54615f767c898599dafa2769c9c66afd5dc67
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-27 15:23:23 -0300

    feat: create user interface

---
+++ src/interfaces/user.ts
@@ -0,0 +1,13 @@
+interface User {
+  name: string;
+  birth: string;
+  cpf: string;
+
+  email: string;
+  password: string;
+
+  cep: string;
+  state: string;
+  city: string;
+  address: string;
+}
