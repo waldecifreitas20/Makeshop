c87574ea969bf012e904adb32e2e00b802267757
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-28 12:16:49 -0300

    refactor: enable input id setter

--- src/components/InputBlock.tsx
+++ src/components/InputBlock.tsx
@@ -3,6 +3,7 @@ import { ResponsibleInput } from "./ResponsibleInput";
 
 interface InputBlockProps {
   label: string,
+  inputId: string,
   type?: string,
   placeholder?: string,
   margins?:string,
@@ -10,7 +11,7 @@ interface InputBlockProps {
 
 export function InputBlock(props: InputBlockProps) {
   return <div className={`${props.margins ?? "my-2 lg:mx-2"} w-full`}>
-    <label htmlFor="">{props.label}</label>
-      <ResponsibleInput type={props.type ?? "text"} placeholder={props.placeholder ?? ""} />
+    <label htmlFor={props.inputId}>{props.label}</label>
+      <ResponsibleInput id={props.inputId} type={props.type ?? "text"} placeholder={props.placeholder ?? ""} />
   </div>
 }
