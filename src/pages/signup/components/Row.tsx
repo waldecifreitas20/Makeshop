df0f772d55f4c2dbb490a179add7583c2a36e31a
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-29 13:10:34 -0300

    fix: allow row styling outside

--- src/pages/signup/components/Row.tsx
+++ src/pages/signup/components/Row.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 
 export function Row(props: PropsWithChildren<{ style?: string }>) {
-  return <div className={`flex w-full gap-4`}>
+  return <div className={`${props.style ?? ""} flex w-full gap-4`}>
     {props.children}
   </div>
 }
