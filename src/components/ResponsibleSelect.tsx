e4083aeae49488d8cad38722cff657322386db41
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-05-29 17:02:29 -0300

    reafactor: remove unused imports

--- src/components/ResponsibleSelect.tsx
+++ src/components/ResponsibleSelect.tsx
@@ -1,27 +1,28 @@
-import React, { Children, PropsWithChildren, ReactElement, ReactNode } from "react";
+import React, { Children, PropsWithChildren, ReactElement } from "react";
 
-const responsibleClass = `
-  block 
-  border border-zinc-400 rounded-full 
-  w-full 
-  px-5 py-2 
-  text-lg
-  outline-black
 
-  md:py-2
-  md:text-sm
-  md:px-2
-
-  lg:rounded-md
-
-`
 interface ResponsibleComponent { }
 
 export function ResponsibleComponent(props: PropsWithChildren<any>) {
+  
+  const responsibleClass = `
+    block 
+    border border-zinc-400 rounded-full 
+    w-full 
+    px-5 py-2 
+    text-lg
+    outline-black
+  
+    md:py-2
+    md:text-sm
+    md:px-2
+  
+    lg:rounded-md
+  `
   const responsibleChildren = Children.map(props.children, (child: ReactElement) => {
     const responsibleElement = React.cloneElement(child, { className: responsibleClass });
 
-    return <><div>{responsibleElement}</div></>
+    return <>{responsibleElement}</>
   });
 
   return <>{responsibleChildren}</>
