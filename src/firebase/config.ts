9653349ac57c367376e4ef401bf6e62b04f14080
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-06-10 09:26:32 -0300

    fix: delete ghost code

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,12 +6,3 @@ export const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
-
-const firebaseConfig2 = {
-  apiKey: "AIzaSyAQMN9SPW04-bGD29yTFqbXQG-MAVkeAIo",
-  authDomain: "web-apps-production-ad82f.firebaseapp.com",
-  projectId: "web-apps-production-ad82f",
-  storageBucket: "web-apps-production-ad82f.appspot.com",
-  messagingSenderId: "350181233394",
-  appId: "1:350181233394:web:f7f361543cb50aab05b66b"
-};
