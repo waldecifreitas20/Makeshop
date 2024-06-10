35d532bb16b3068879d8ab3a9998931f5d87843b
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-06-10 09:26:54 -0300

    fix: delete ghost code

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,21 +2,8 @@ import { getFirestore, setDoc, doc, getDoc } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './config';
 
-console.log(firebaseConfig);
-
-
-const firebaseConfig2 = {
-  apiKey: "AIzaSyAQMN9SPW04-bGD29yTFqbXQG-MAVkeAIo",
-  authDomain: "web-apps-production-ad82f.firebaseapp.com",
-  projectId: "web-apps-production-ad82f",
-  storageBucket: "web-apps-production-ad82f.appspot.com",
-  messagingSenderId: "350181233394",
-  appId: "1:350181233394:web:f7f361543cb50aab05b66b"
-};
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 
 async function createDocument(collectionName: string, docKey: string, docFields: object) {
   try {
