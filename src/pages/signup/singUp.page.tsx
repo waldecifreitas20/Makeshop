3043479de87ead0c0174035f2c0e0333eca375f8
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-06-16 16:20:06 -0300

    replace input block by label block

--- src/pages/signup/singUp.page.tsx
+++ src/pages/signup/singUp.page.tsx
@@ -1,47 +1,91 @@
-import { ReactElement, useState } from "react";
+import { useState } from "react";
 import { BackHomeButton } from "../../components/BackHomeButton";
-import { InputBlock } from "../../components/InputBlock";;
+
 import { ResponsibleButton } from "../../components/ResponsibleButton";
 import { formUtils } from "../../utils/forms";
 import { Row } from "./components/Row";
-import { signUpMethods } from "./signUp.methods";
-import { ResponsibleComponent } from "../../components/ResponsibleSelect";
+import { userServices } from "../../services/user.services";
+import { ResponsibleInput } from "../../components/ResponsibleInput";
+import { LabelBlock } from "./components/LabelBlock";
+import { ResponsibleComponent } from "../../components/ResponsibleComponent";
 
-interface InputBlockProps {
-  label: string,
-  child: ReactElement,
-  placeholder?: string,
-};
 
-export function LabelBlock(props: InputBlockProps) {
-  return <>
-    <div className="w-full">
-      <label>{props.label}</label>
-      {props.child}
-    </div>
-  </>
+function validateForm(userData: any,
+  onInvalid: (
+    message: string,
+    input: HTMLInputElement,
+  ) => void): boolean {
+
+  const { result: hasEmptyFields, emptyInput } = formUtils.checkEmptyFields(userData);
+  if (hasEmptyFields) {
+    onInvalid("Preencha todos os campos", emptyInput);
+    return false;
+  }
+
+  if (!formUtils.isValidEmail(userData.email.value)) {
+    onInvalid("Digite um email válido", userData.email);
+    return false;
+  }
+
+  if (!formUtils.isValidPassword(userData.password.value, true)) {
+    onInvalid("Senha precisa conter entre 8 e 16 caracteres", userData.password);
+    return false;
+  }
+
+  return true;
+}
+
+async function signUp(userData: any) {
+  const user: User = {
+    name: userData.name.value,
+    address: userData.address.value,
+    birth: userData.birth.value,
+    cep: userData.cep.value,
+    city: userData.city.value,
+    cpf: userData.cpf.value,
+    email: userData.email.value,
+    password: userData.password.value,
+    state: userData.state.value,
+  }
+
+  return await userServices.createUser(user);
+}
+
+function getStatesNames() {
+  return [
+    'pará', 'são paulo', 'mato grosso',
+    'mato grosso do sul', 'rio de janeiro',
+    'maranhão', 'acre', 'amapá', 'rondônia',
+    'roraima', 'tocantins', 'piauí',
+    'paraná', 'distrito federal', 'goiás',
+    'bahia', 'paraíba', 'pernambuco',
+    'rio grande do norte', 'ceará', 'alagoas',
+    'rio grande do sul', 'sergipe', 'santa catarina',
+    'espirito santo', 'minas gerais', 'amazonas'
+  ];
 }
 
 
 export function SignUpPage() {
-
+  /* STATES */
   const [isLoading, setLoaderState] = useState(false);
+  const [userData, setUserData] = useState({
+    name: "",
+    address: "",
+    birth: "",
+    cep: "",
+    city: "",
+    cpf: "",
+    email: "",
+    password: "",
+    state: "",
+  });
 
   const onSubmit = async (event: React.MouseEvent) => {
-    const formFields = {
-      name: formUtils.getField("signup-form", "name"),
-      address: formUtils.getField("signup-form", "address"),
-      birth: formUtils.getField("signup-form", "birth-date"),
-      cep: formUtils.getField("signup-form", "cep"),
-      city: formUtils.getField("signup-form", "city"),
-      cpf: formUtils.getField("signup-form", "cpf"),
-      email: formUtils.getField("signup-form", "email"),
-      password: formUtils.getField("signup-form", "password"),
-      state: formUtils.getField("signup-form", "state-select"),
-    }
+
     event.preventDefault();
 
-    const isValid = signUpMethods.validateForm(formFields, (message, input) => {
+    const isValid = validateForm(userData, (message, input) => {
       alert(message);
       formUtils.setInvalidInput(input);
       event.preventDefault();
@@ -50,7 +94,7 @@ export function SignUpPage() {
 
     if (isValid) {
       setLoaderState(true);
-      signUpMethods.signUp(formFields)
+      signUp(userData)
         .then(console.log)
         .catch(() => {
           alert("Usuário já cadastrado! Faça o login em vez disto.")
@@ -68,41 +112,132 @@ export function SignUpPage() {
 
       <form id="signup-form" className="md:max-w-[700px] mx-auto" method="GET" action="/">
         <fieldset>
-          <InputBlock inputId="name" label="Nome Completo" placeholder="Ex:José Ribamar da Silva" />
-          <Row style="flex-wrap md:flex-nowrap">
-            <InputBlock inputId="cpf" label="CPF" placeholder="xxx.xxx.xxx-xx" />
-            <InputBlock inputId="birth-date" label="Data de Nascimento" placeholder="dd/mm/aa" type="date" />
+          <LabelBlock label="Nome Completo">
+            <ResponsibleInput
+              id="name"
+              type="text"
+              placeholder="Ex: José Ribamar da Silva"
+              onChange={(event => {
+                setUserData({
+                  ...userData,
+                  name: event.target.value,
+                })
+              })}
+            />
+          </LabelBlock>
+
+          <Row style="flex-wrap">
+
+            <LabelBlock label="CPF">
+              <ResponsibleInput
+                id="cpf"
+                type="text"
+                placeholder="xxx.xxx.xxx-xx"
+                onChange={(event => {
+                  setUserData({
+                    ...userData,
+                    cpf: event.target.value,
+                  })
+                })}
+              />
+            </LabelBlock>
+
+            <LabelBlock label="Data de Nascimento">
+              <ResponsibleInput placeholder="dd/mm/aa" type="date"
+                onChange={(event => {
+                  setUserData({
+                    ...userData,
+                    cpf: event.target.value,
+                  })
+                })} />
+            </LabelBlock>
           </Row>
         </fieldset>
 
         <fieldset className="my-8">
+          <LabelBlock label="CEP">
+            <ResponsibleInput placeholder="00000-000"
+              onChange={(event => {
+                setUserData({
+                  ...userData,
+                  cep: event.target.value,
+                })
+              })} />
+          </LabelBlock>
 
-          <InputBlock inputId="cep" label="CEP" placeholder="00000-000" />
-
-          <Row style="md:flex items-end">
+          <Row style="flex-wrap">
             <ResponsibleComponent style="w-[100%]">
-
               <label htmlFor="state-select">Estado</label>
-
-              <select id="state-select">
+              <select id="state-select" onChange={(event => {
+                setUserData({
+                  ...userData,
+                  state: event.target.value,
+                })
+              })}>
                 <option value="null">--- Selecione ---</option>
-                {signUpMethods.getStatesNames().map((state, _) => {
+                {getStatesNames().map((state, _) => {
                   return <>
                     <option value={state}>{state.toUpperCase()}</option>
                   </>
                 })}
               </select>
-
             </ResponsibleComponent>
-            <InputBlock margins="" inputId="city" label="Cidade" placeholder="00000-000" />
+
+            <LabelBlock label="Cidade">
+              <ResponsibleInput placeholder="00000-000"
+                onChange={(event => {
+                  setUserData({
+                    ...userData,
+                    city: event.target.value,
+                  })
+                })} />
+            </LabelBlock>
           </Row>
 
-          <InputBlock inputId="address" label="Endereço" placeholder="Rua Castro 475 B" />
+          <LabelBlock label="Endereço">
+            <ResponsibleInput placeholder="Rua Castro 475 B"
+              onChange={(event => {
+                setUserData({
+                  ...userData,
+                  address: event.target.value,
+                })
+              })}
+            />
+          </LabelBlock>
         </fieldset>
 
         <fieldset>
-          <InputBlock inputId="email" label="Email" placeholder="joseribamar23@outlook.com" type="email" />
-          <InputBlock inputId="password" label="Senha" placeholder="Minímo 8 caracteres" type="password" />
+          <LabelBlock label="email">
+            <ResponsibleInput placeholder="joseribamar23@outlook.com" type="email"
+              onChange={(event => {
+                setUserData({
+                  ...userData,
+                  email: event.target.value,
+                })
+              })} />
+          </LabelBlock>
+
+          <Row style="flex-wrap">
+            <LabelBlock label="senha">
+              <ResponsibleInput placeholder="Minímo 8 caracteres" type="password"
+                onChange={(event => {
+                  setUserData({
+                    ...userData,
+                    password: event.target.value,
+                  })
+                })} />
+            </LabelBlock>
+
+            <LabelBlock label="confirme sua senha">
+              <ResponsibleInput placeholder="Minímo 8 caracteres" type="password"
+                onChange={(event => {
+                  setUserData({
+                    ...userData,
+                    password: event.target.value,
+                  })
+                })} />
+            </LabelBlock>
+          </Row>
 
           <ul className="mx-2">
             <li className="text-green-500"><span>X</span> Contém simbolo</li>
