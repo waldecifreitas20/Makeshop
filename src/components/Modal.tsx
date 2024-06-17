b4f03d156d7a1266ba4cb9047462836d94a6ce67
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-06-17 15:47:18 -0300

    feat: modal responsible style

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,11 @@
 import { PropsWithChildren } from "react";
 
 export function Modal(props: PropsWithChildren) {
-
+  
   return <>
     <div className="fixed top-0 h-screen w-full bg-black bg-opacity-20 flex justify-center items-center">
       {/* Modal */}
-      <div className="relative border  bg-white w-[300px] h-[200px]">
+      <div className="relative border bg-white px-4 py-5 rounded-lg w-[90%] sm:w-[70%] md:max-w-[500px]">
         {props.children}
       </div>
     </div>
