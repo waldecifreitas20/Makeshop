69e7f0056af4d7a092d40050e094b918f4d33c89
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-06-17 17:21:24 -0300

    fix: remove preset width to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,18 @@
 import { PropsWithChildren } from "react";
 
-export function Modal(props: PropsWithChildren) {
-  
+export function Modal(props: PropsWithChildren<{ style?: string }>) {
+
   return <>
     <div className="fixed top-0 h-screen w-full bg-black bg-opacity-20 flex justify-center items-center">
       {/* Modal */}
-      <div className="relative border bg-white px-4 py-5 rounded-lg w-[90%] sm:w-[70%] md:max-w-[500px]">
+      <div className={`
+        relative 
+        border rounded-lg 
+        bg-white 
+        px-4 py-2 
+        
+        ${props.style ?? ""}
+      `}>
         {props.children}
       </div>
     </div>
