8544c3cac9a2c4092d63584a123fd70b872a0c82
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-06-18 10:18:50 -0300

    feat: provisory login state

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,10 +1,13 @@
 import { firebase } from "../firebase";
 import users from "../mocks/users.json";
 
+let isAuthenticated = false;
+
 async function authenticate(email: string, password: string) {
 
   for (const user of users) {
     if (user.email === email && user.password === password) {
+      isAuthenticated = true;
       return {
         token: Math.random().toString(),
         user,
@@ -15,16 +18,21 @@ async function authenticate(email: string, password: string) {
   throw Error("Email e/ou Senha inválidos");
 }
 
+async function hasAuthenticated() {
+  return true;
+}
+
 async function signUp(user: User) {
   const hasUser = await firebase.hasDocument("users", user.email);
   if (hasUser) {
     throw Error("Usuário já Existe");
   }
-
-  return await firebase.createDocument("users", user.email, user);
+  await firebase.createDocument("users", user.email, user);
+  return
 }
 
 export const userServices = {
   authenticate,
+  hasAuthenticated,
   signUp,
 }
