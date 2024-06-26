28308a891219c796af7b40a238de46be33d2a03a
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-06-25 21:34:48 -0300

    refactor: product info component

---
+++ src/components/ProductInfo.tsx
@@ -0,0 +1,36 @@
+import { appColors } from "../global/colors";
+
+interface ProductInfoProps {
+  product: Product;
+  isDark?: boolean;
+}
+
+export function ProductInfo(props: ProductInfoProps) {
+
+  function getTextTheme() {
+    if (!props.isDark) {
+      return "text-black";
+    }
+
+    return "text-yellow-500";
+  }
+
+  return (
+    <>
+      <div>
+        {/* Old price */}
+        <p className={`text-sm ${appColors.texts.dimmed} text-line-through`}>{(() => {
+          let oldPrice: number = props.product.price * 1.25;
+          return <>{`R$ ${oldPrice.toFixed(2)}`}</>;
+        })()}</p>
+
+        {/* Current Price */}
+        <p className={`${getTextTheme()} text-lg font-bold`
+        }>{`R$ ${props.product.price.toFixed(2)}`}</p>
+
+        {/* With credit card */}
+        <p className={`${getTextTheme()} text-sm`}>{`ou 10x de R$ ${(props.product.price / 10).toFixed(2)}`}</p>
+      </div>
+    </>
+  );
+} 
