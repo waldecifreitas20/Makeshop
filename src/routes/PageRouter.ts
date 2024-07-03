cfbb02fd3b6a054b18898ededd550189190fa0cd
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-07-03 16:34:31 -0300

    fix: app routing

--- src/routes/PageRouter.ts
+++ src/routes/PageRouter.ts
@@ -1,8 +1,5 @@
 function goTo(routePath: string) {
-  const trash = `${window.location.pathname}${window.location.search}`;
-  const url = window.location.href.replace(trash, "");
-
-  window.location.href = `${url}${routePath}`;
+  window.location.href = routePath;
 }
 
 export const PageRouter = {
