73d50d89e5ced0dd7d04c03e43b7dd424331acc5
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-07-15 19:07:19 -0300

    feat: payment page full responsive

--- src/pages/payment/payment.page.tsx
+++ src/pages/payment/payment.page.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { Navbar } from "../../components/Navbar";
 import { paymentServices } from "../../services/payment.services";
 import { ResponsibleButton } from "../../components/ResponsibleButton";
+import { Toast } from "../../components/Toast";
+import { toast } from "react-toastify";
+import { PageRouter } from "../../routes/PageRouter";
+import { routes } from "../../routes/routes";
 
 export function PaymentPage() {
   const [qrCodeUrl, setQrCodeUrl] = useState("");
@@ -10,24 +14,43 @@ export function PaymentPage() {
     paymentServices.generateQRCode().then((url) => {
       setQrCodeUrl(url);
     });
-  });
+  }, []);
 
   return (
     <>
       <Navbar />
-      <main>
-        <h2>Quase lá</h2>
-        <section>
-          <h3>Escaneie o QR Code</h3>
-          <img src={qrCodeUrl} alt="" />
+
+      <Toast />
+
+      <main className="px-10 max-w-[500px] mx-auto">
+        <h2 className="text-4xl">Quase lá!</h2>
+
+        <section className="mt-7 text-center flex justify-start items-center flex-col">
+          <h3 className="text-xl mb-4">Escaneie o QR Code</h3>
+          <img className="block w-full max-w-[300px]" src={qrCodeUrl} alt="qr code" />
 
           <p>ou copie o codigo e cole no app do seu banco</p>
-          <ResponsibleButton>Copiar codigo</ResponsibleButton>
+
+          <ResponsibleButton
+            background="bg-green-300 hover:bg-green-500"
+            textColor="text-black"
+            onClick={() => {
+              navigator.clipboard.writeText(qrCodeUrl);
+              toast("código copiado!");
+            }}>
+            Copiar codigo
+          </ResponsibleButton>
+
         </section>
       </main>
 
-      <footer>
-        <ResponsibleButton>Voltar às compras</ResponsibleButton>
+      <footer className="px-10 max-w-[500px] mx-auto">
+        <ResponsibleButton
+          background="bg-pink-400 hover:bg-pink-600"
+          onClick={() => {
+            PageRouter.goTo(routes.home);
+          }}
+        >Voltar às compras</ResponsibleButton>
       </footer>
     </>
   );
