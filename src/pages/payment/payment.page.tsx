29b0e8f7c73222719e5ce093efb782f25b13b537
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-07-15 18:44:33 -0300

    feat: payment page basic structure

--- src/pages/payment/payment.page.tsx
+++ src/pages/payment/payment.page.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import { Navbar } from "../../components/Navbar";
+import { paymentServices } from "../../services/payment.services";
+import { ResponsibleButton } from "../../components/ResponsibleButton";
 
 export function PaymentPage() {
+  const [qrCodeUrl, setQrCodeUrl] = useState("");
+
+  useEffect(() => {
+    paymentServices.generateQRCode().then((url) => {
+      setQrCodeUrl(url);
+    });
+  });
+
   return (
     <>
       <Navbar />
-      <main></main>
+      <main>
+        <h2>Quase lá</h2>
+        <section>
+          <h3>Escaneie o QR Code</h3>
+          <img src={qrCodeUrl} alt="" />
+
+          <p>ou copie o codigo e cole no app do seu banco</p>
+          <ResponsibleButton>Copiar codigo</ResponsibleButton>
+        </section>
+      </main>
+
+      <footer>
+        <ResponsibleButton>Voltar às compras</ResponsibleButton>
+      </footer>
     </>
   );
 }
